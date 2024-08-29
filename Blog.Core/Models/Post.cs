836f9e4e8b0f6e98c528662cf13862c095ba291d
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-08-29 13:15:33 +0300

    Post Modeli Slug yap

--- Blog.Core/Models/Post.cs
+++ Blog.Core/Models/Post.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Blog.Core.Models
 {
-    public class Post
+    public class Post:BaseModel
     {
+
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime DatePublished { get; set; }
+        public bool IsPublished { get; set; }
+        public string Tags { get; set; }
+
+        //SLUG EKLE 
     }
 }
