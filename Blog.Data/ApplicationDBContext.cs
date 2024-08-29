663b246101a3b8b09aa7ab3aad5fc812649bb303
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-08-29 13:48:06 +0300

    Program cs APP DB CONTEXT

--- Blog.Data/ApplicationDBContext.cs
+++ Blog.Data/ApplicationDBContext.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Blog.Core.Models;
+using System.Drawing;
 
 namespace Blog.Data
 {
-    public class ApplicationDBContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
+
--- Blog.Web/Program.cs
+++ Blog.Web/Program.cs
@@ -1,7 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Blog.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//// Register repository implementations
+//builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+// Add HttpContextAccessor
+builder.Services.AddHttpContextAccessor();
+
+// Configure authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/User/Login";
+        options.LogoutPath = "/User/Login";
+        options.Cookie.Name = "MuratBlogCookie";
+        options.SlidingExpiration = true;
+    });
+
+//// Register services with DI
+//builder.Services.BusinessDI();
+//builder.Services.RepositoryDI();
 
 var app = builder.Build();
 
@@ -9,7 +38,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,7 +46,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
