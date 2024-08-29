a250a85ea58e3e9c14b1f818303aceb1342ca33d
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-08-29 13:12:50 +0300

    BaseModel + User Model.

---
+++ Blog.Core/Models/BaseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Models
+{
+    public class BaseModel
+    {
+
+        public int Id { get; set; }
+        public Guid GuidId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+       
+    }
+}
--- Blog.Core/Models/User.cs
+++ Blog.Core/Models/User.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Blog.Core.Models
 {
-    public class User
+    public class User:BaseModel
     {
+
+        public string UserName { get; set; }
+        //Passwordu Hash Yap gizilee
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int DateOfBirth { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string ProfilePictureUrl { get; set; }
+        public bool IsAdmin { get; set; }   
+
     }
 }
