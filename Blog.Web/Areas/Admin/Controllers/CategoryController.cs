c70e307e4f718048386f75786ffcc8b5da024bea
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-09-06 18:42:29 +0300

    .

--- Blog.Web/Areas/Admin/Controllers/CategoryController.cs
+++ Blog.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -1,11 +1,11 @@
 ﻿using Blog.Business.Absract;
-
 using Blog.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("Admin/[controller]")]
     public class CategoryController : Controller
     {
         private readonly ICategoryService _categoryService;
@@ -15,18 +15,21 @@ namespace Blog.Web.Areas.Admin.Controllers
             _categoryService = categoryService;
         }
 
+        // GET: Admin/Category
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpGet("Category/GetAll")]
-        public IActionResult GetAll()
+        // GET: Admin/Category/GetAllCategories
+        [HttpGet("GetAllCategories")]
+        public IActionResult GetAllCategories()
         {
             try
             {
                 var categories = _categoryService.GetAll();
-                return Ok(categories);
+                return Json(categories);
             }
             catch (Exception ex)
             {
@@ -34,7 +37,8 @@ namespace Blog.Web.Areas.Admin.Controllers
             }
         }
 
-        [HttpPost("Category/Add")]
+        // POST: Admin/Category/Add
+        [HttpPost("Add")]
         public IActionResult Add([FromBody] Category category)
         {
             try
@@ -48,7 +52,8 @@ namespace Blog.Web.Areas.Admin.Controllers
             }
         }
 
-        [HttpPost("Category/Update/{id}")]
+        // PUT: Admin/Category/Update
+        [HttpPut("Update")]
         public IActionResult Update([FromBody] Category category)
         {
             if (category == null)
@@ -66,10 +71,11 @@ namespace Blog.Web.Areas.Admin.Controllers
             }
         }
 
-        [HttpGet("Category/GetById/{id}")]
-        public IActionResult GetById(Guid id)
+        // GET: Admin/Category/GetById/{id}
+        [HttpGet("GetById/{id}")]
+        public IActionResult GetById(int id)
         {
-            if (id == Guid.Empty)
+            if (id == 0) // 'id == null' olmaz, çünkü id bir integer.
             {
                 return BadRequest("Invalid Id Format");
             }
@@ -88,7 +94,8 @@ namespace Blog.Web.Areas.Admin.Controllers
             }
         }
 
-        [HttpDelete("Category/Delete/{id}")]
+        // DELETE: Admin/Category/Delete/{id}
+        [HttpDelete("Delete/{id}")]
         public IActionResult Delete(Guid id)
         {
             if (id == Guid.Empty)
