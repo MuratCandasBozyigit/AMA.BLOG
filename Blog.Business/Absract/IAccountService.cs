3f0b7c2d0f9c8ae80acdfe1942bf2f023057fe85
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-09-08 22:06:42 +0300

    Ne yaptığımı bende şaşırdım İnternetim olmasaydıda ChatGpt ye sormasaydım keşke en küüccük şeyi bile allah belamı verdi yazdığım kodun neresinin ne olduğunu şaşırdım kafam kalmadı MURAT OKURSAN APTALLIK ETME İLERİDE YAPMA KULLANMA Aİ DAHA KÖTÜLER YETERLİ DEĞİLLER OLSALARDI İŞ YAPANLAR İNSAN OLMAZDI MK KAFAYI ÜŞÜTTÜM YEMİN EDERİM 10 DK DE YAPACAĞIM İŞLERİ TÜM GÜN YAPAMIYOM FITTIRDIM YAPMA MURAT YAPMA OKUYACAKSIN BU COMMİTLERİ Aİ KULLANMA KEŞKE DAHADA BÜYÜK YAZILSA Aİ Aİ Aİ KULLANMAAAA KÜFÜR EDECEM DElİRDİM

--- Blog.Business/Absract/IAccountService.cs
+++ Blog.Business/Absract/IAccountService.cs
@@ -1,10 +1,12 @@
 ﻿using System.Threading.Tasks;
-using Blog.Business.Shared.Absract;
 using Blog.Core.Models;
 using Blog.Data.Shared.Abstract;
 using Blog.Web.Models;
 using Microsoft.AspNetCore.Identity;
 
-public interface IAccountService : IService<AppUser>
+public interface IAccountService
 {
+    Task<IdentityResult> RegisterUserAsync(RegisterViewModel model);
+    Task<SignInResult> LoginUserAsync(LoginViewModel model);
+    Task LogoutUserAsync();
 }
