ced8f253d0cc89794b0dfc46f854416c9c3f75cf
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-09-08 14:27:56 +0300

    AppUserModelUpt

--- Blog.Core/Models/AppUser.cs
+++ Blog.Core/Models/AppUser.cs
@@ -7,18 +7,16 @@ public class AppUser : BaseModel
     public int? AppUserId { get; set; }
     //Passwordu Hash Yap gizilee
     public string Password { get; set; }
-    public string Email { get; set; }
-
-
-    public int DateOfBirth { get; set; }
+    public string Email { get; set; } 
+  
     public DateTime CreatedDate { get; set; } = DateTime.Now;
 
-    public string ProfilePictureUrl { get; set; }
+    public string? ProfilePictureUrl { get; set; }
 
     public bool IsAdmin { get; set; }
-
-
-   
- 
-
 }
+
+//Gereksiz zaten hesap acılmadan okunulabilinyor ?D
+
+////Yaşa göre içerik olabilir diye yaş aldım bunun aşagıda calışmaya başladıktan sonra kodu ayarla hesap yapsın kullanıcı 18 den veya 13 ten kücükse statement ile hallet home ındexte gözükecek itemler ona göre listelensin boş ise ındex uygun içerik yok gibi bir yazılı sayfaya yada içerik henüz yok giib bir sayfaya yonlendrilsin 
+//public int DateOfBirth { get; set; }
