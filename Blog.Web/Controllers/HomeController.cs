ee6e624fc46f7077fd821204d48b66406ab6fae3
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-09-28 02:23:36 +0300

    PostDetail View+css comment ekle tasarımı düzenle

--- Blog.Web/Controllers/HomeController.cs
+++ Blog.Web/Controllers/HomeController.cs
@@ -16,27 +16,29 @@ namespace Blog.Web.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IPostService _postService;
         private readonly ICategoryService _categoryService;
-
-        public HomeController(ILogger<HomeController> logger, IPostService postService, ICategoryService categoryService)
+        private readonly ITagService _tagService;
+        public HomeController(ILogger<HomeController> logger, IPostService postService, ICategoryService categoryService, ITagService tagService)
         {
             _logger = logger;
             _postService = postService;
             _categoryService = categoryService;
+            _tagService = tagService;
         }
 
 
-
+        //Etiketleride listeleyebilrsin istersen tagı getirdim.
         public async Task<IActionResult> Index()
         {
             try
             {
                 var posts = _postService.GetAllPosts(); // Senkron şekilde postları al
                 var categories = _categoryService.GetAllCategories(); // Senkron şekilde kategorileri al
-
+                var tags = _tagService.GetAll();
                 var model = new HomeViewModel
                 {
                     Posts = posts,
-                    Categories = categories
+                    Categories = categories,
+                    Tags = tags
                 };
 
                 return View(model); // View'a model ile birlikte gönder
@@ -48,11 +50,54 @@ namespace Blog.Web.Controllers
             }
         }
 
+        public IActionResult PostDetails(int id)
+        {
+            var post = _postService.GetById(id); 
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+
+
+        //public async Task<IActionResult> GetPostContent(Post post)
+        //{
+        //    try
+        //    {
+        //        var posts = _postService.GetAllPosts(); // Senkron şekilde postları al
+        //        var categories = _categoryService.GetAllCategories(); // Senkron şekilde kategorileri al
+        //        var tags = _tagService.GetAll();
+
+        //        var model = new ContentViewModel
+        //        {
+        //            Posts = posts,
+        //            Categories = categories,
+        //            Tags = tags
+
+        //        };
+        //        return View(model);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(ex, "Post içeriği yüklenirken hata oluştu.");
+        //        return View("Error"); // Hata sayfasını döndür
+        //    };
+        //}
+
+        public class ContentViewModel
+        {
+            public IEnumerable<Post> Posts { get; set; }
+            public IEnumerable<Category> Categories { get; set; }
+            public IEnumerable<Tag> Tags { get; set; }
+        }
 
         public class HomeViewModel
         {
             public IEnumerable<Post> Posts { get; set; }
             public IEnumerable<Category> Categories { get; set; }
+            public IEnumerable<Tag> Tags { get; set; }
         }
 
 
