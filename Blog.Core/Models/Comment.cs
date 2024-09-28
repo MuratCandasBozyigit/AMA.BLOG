d3c31535287f2745957d5faff6a918521fe599f0
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-09-28 22:35:55 +0300

    .

--- Blog.Core/Models/Comment.cs
+++ Blog.Core/Models/Comment.cs
@@ -1,8 +1,20 @@
-﻿using Blog.Core.Models;
+﻿using System;
+using Microsoft.AspNetCore.Identity;
 
-public class Comment : BaseModel
+namespace Blog.Core.Models
 {
-    public string Content { get; set; }
-    public string AuthorId { get; set; } // AppUser ID olarak değiştirin
-    public DateTime DateCommented { get; set; }
+    public class Comment : BaseModel
+    {
+        public string Content { get; set; }
+
+        // Identity kullanıcı sınıfıyla ilişkilendirilmiş Author
+        public string AuthorId { get; set; }
+        public AppUser Author { get; set; } // AppUser sınıfı ile ilişkilendirilmiş
+
+        public DateTime DateCommented { get; set; } = DateTime.Now; // Varsayılan olarak yorumun yapıldığı tarih
+
+        // İlgili Post ile ilişkilendirme
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
 }
