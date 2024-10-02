e3758b2e1674941bc562e163432381809dc1ce56
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-10-02 15:13:41 +0300

    Identity Role Modelini EKledim

--- Blog.Core/Models/AppUser.cs
+++ Blog.Core/Models/AppUser.cs
@@ -3,19 +3,21 @@ using Microsoft.AspNetCore.Identity;
 
 public class AppUser : IdentityUser
 {
-    //Passwordu Hash Yap gizilee
+    
     public string FullName { get; set; }
-    //public string Password { get; set; }
-    //public string Email { get; set; } 
-  
+ 
     public DateTime CreatedDate { get; set; } = DateTime.Now;
 
     public string? ProfilePictureUrl { get; set; }
 
     public bool IsAdmin { get; set; } = false;
+  
 }
+
+
 
 //Gereksiz zaten hesap acılmadan okunulabilinyor ?D
 
 ////Yaşa göre içerik olabilir diye yaş aldım bunun aşagıda calışmaya başladıktan sonra kodu ayarla hesap yapsın kullanıcı 18 den veya 13 ten kücükse statement ile hallet home ındexte gözükecek itemler ona göre listelensin boş ise ındex uygun içerik yok gibi bir yazılı sayfaya yada içerik henüz yok giib bir sayfaya yonlendrilsin 
-//public int DateOfBirth { get; set; }
+//public int DateOfBirth { get; set; }   //public string Password { get; set; }
+//public string Email { get; set; } //Passwordu Hash Yap gizilee
---
+++ Blog.Core/Models/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Core.Models
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public bool IsAdmin { get; set; }
+       // public bool IsModerator { get; set; }
+        public string Description { get; set; }
+    }
+}
