a482a1edddb30b3a428e8e72ef9e676be98e346d
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-10-02 15:01:00 +0300

    .

---
+++ Blog.Web/Areas/Admin/Controllers/UserListController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Areas.Admin.Controllers
+{
+    public class UserListController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserListController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+    }
+}
