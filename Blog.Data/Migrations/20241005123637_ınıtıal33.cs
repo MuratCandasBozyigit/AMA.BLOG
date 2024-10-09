3ffb400978134f3fd7590dd6c7a4cd05ecb4d1b6
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-10-09 15:43:24 +0300

    .

---
+++ Blog.Data/Migrations/20241005123637_ınıtıal33.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ınıtıal33 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ParentCategoryName",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentCategoryName",
+                table: "Categories");
+        }
+    }
+}
