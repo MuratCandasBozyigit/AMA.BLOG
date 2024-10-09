c64ca2d35fd8d6388c05322166f9b07b96b85939
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-10-09 19:15:51 +0300

    Oldurdum link erinde slug değil tagName yazıyor artık

--- Blog.Web/Program.cs
+++ Blog.Web/Program.cs
@@ -73,7 +73,7 @@ app.UseAuthorization();
 //);
 app.MapControllerRoute(
     name: "PostDetail",
-    pattern: "postdetail/{category}/{slug}/{id}",
+    pattern: "postdetail/{category}/{tag}/{id}",
     defaults: new { controller = "PostDetail", action = "Index" }
 );
 
